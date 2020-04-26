d5c9149aa2b80fc39d2e997cc007ee83c6df804a
PaulGutkovich <paul.mte5@gmail.com>
2020-04-25 20:27:41 -0400

    added some stuff to train.py

--- train.py
+++ train.py
@@ -1,1 +1,54 @@
-import tensorflow.keras
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Dropout, Flatten, Dense
+from tensorflow.keras.callbacks import LearningRateScheduler
+
+plt.set_cmap("binary")
+
+train = pd.read_csv("train.csv")
+y = train["label"].values.reshape(-1, 1)
+X_train = train.drop(["label"], axis=1).values.reshape(-1, 28, 28, 1)/255.
+
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.imshow(X_train[i])
+    plt.title(y[0])
+    plt.show()
+
+ohe = OneHotEncoder()
+y = ohe.fit_transform(y)
+print(y[0])
+
+datagen = ImageDataGenerator(rotation_range=10, zoom_range=0.10, width_shift_range=0.1, height_shift_range=0.1)
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape = (28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, kernel_size=5, strides=2, padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size=5, strides=2, padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Conv2D(128, kernel_size=4, activation='relu'))
+model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dropout(0.4))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
