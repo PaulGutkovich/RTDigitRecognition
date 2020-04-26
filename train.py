d4aa131ffa55b5d26fdfcaa626711c82bd77f850
PaulGutkovich <paul.mte5@gmail.com>
2020-04-26 11:01:19 -0400

    completed the CNN model

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from keras.utils.np_utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Dropout, Flatten, Dense
@@ -12,17 +14,16 @@ plt.set_cmap("binary")
 
 train = pd.read_csv("train.csv")
 y = train["label"].values.reshape(-1, 1)
-X_train = train.drop(["label"], axis=1).values.reshape(-1, 28, 28, 1)/255.
+X = train.drop(["label"], axis=1).values.reshape(-1, 28, 28, 1)/255.
 
 for i in range(9):
     plt.subplot(3, 3, i+1)
-    plt.imshow(X_train[i])
-    plt.title(y[0])
-    plt.show()
+    plt.imshow(X[i].reshape(28, 28))
+    plt.title(y[i])
 
-ohe = OneHotEncoder()
-y = ohe.fit_transform(y)
-print(y[0])
+plt.show()
+
+y = to_categorical(y)
 
 datagen = ImageDataGenerator(rotation_range=10, zoom_range=0.10, width_shift_range=0.1, height_shift_range=0.1)
 
@@ -52,3 +53,12 @@ model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
+lr = LearningRateScheduler(lambda x: 0.01*0.95**x)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=64), epochs=30,
+                              steps_per_epoch=X_train.shape[0]//64, validation_data=(X_val, y_val),
+                              callbacks=[lr], verbose=1)
+
+model.save("weights.h5")
